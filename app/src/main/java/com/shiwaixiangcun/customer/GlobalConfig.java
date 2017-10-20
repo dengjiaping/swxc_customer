dc6f81ba762c553fa619345a242ac466c21cf663
xujing <827938508@qq.com>
2017-10-20 17:03:51 +0800

    commit

--- app/src/main/java/com/shiwaixiangcun/customer/GlobalConfig.java
+++ app/src/main/java/com/shiwaixiangcun/customer/GlobalConfig.java
@@ -14,8 +14,6 @@ public class GlobalConfig {
     public static String clientId = "97d7c8d7418b49de8da45cac01615f3e";
     public static String clientSecret = "fd225cc01f034b2d8c76f97190750664";
 
-    //app的一些常量
-    public static int siteID = 20;
     public static String market_01 = "SWSH_MARKET_01";
     public static String home_01 = "SWSH_HOME_01";
     public static String home_02 = "SWSH_HOME_02";
@@ -25,4 +23,9 @@ public class GlobalConfig {
     public static String userInfo = "userInfo";
     public static String isLogin = "isLogin";
     public static String propertyAuth = "propertyAuth";
+
+    //app的一些常量
+    public static int siteID = 20;
+    public static int first_page = 1;
+    public static int page_size = 20;
 }
---
+++ app/src/main/java/com/shiwaixiangcun/customer/adapter/AdapterMerchant.java
@@ -0,0 +1,32 @@
+package com.shiwaixiangcun.customer.adapter;
+
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.shiwaixiangcun.customer.R;
+import com.shiwaixiangcun.customer.model.MerchantListBean;
+import com.shiwaixiangcun.customer.utils.ImageDisplayUtil;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/20.
+ * 商铺Adapter
+ */
+
+public class AdapterMerchant extends BaseQuickAdapter<MerchantListBean.DataBean.ElementsBean, BaseViewHolder> {
+    public AdapterMerchant(@Nullable List<MerchantListBean.DataBean.ElementsBean> data) {
+        super(R.layout.item_merchant, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, MerchantListBean.DataBean.ElementsBean item) {
+        helper.setText(R.id.tv_merch_content, item.getFeature());
+        helper.setTag(R.id.tv_merch_name, item.getName());
+        ImageView imageView = helper.getView(R.id.iv_merch);
+        ImageDisplayUtil.showImageView(mContext, item.getCover(), imageView);
+
+    }
+}
